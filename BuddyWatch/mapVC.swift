5a0a80e298480891e4e66531a03999d6968f0446
nirbhay-py <nnarang1@jpischool.com>
2019-11-17 02:57:31 +0530

    Added map functionality

---
+++ BuddyWatch/mapVC.swift
@@ -0,0 +1,30 @@
+//
+//  mapVC.swift
+//  BuddyWatch
+//
+//  Created by Nirbhay Singh on 17/11/19.
+//  Copyright © 2019 Nirbhay Singh. All rights reserved.
+//
+
+import UIKit
+
+class mapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
